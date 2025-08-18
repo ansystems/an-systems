2e75174e7289d590fd6cf50d53f4ae1c31b7bdd2
Nipun Agarwal <anandp@an-systems.in>
2025-08-18 20:30:08 +0530

    build: use CRA public/index.html; remove standalone root index.html; wire OnePager in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Services from './components/Services';
-import Footer from './components/Footer';
+import OnePager from './components/OnePager';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Hero />
-      <Services />
-      <Footer />
-    </div>
+    <OnePager />
   );
 }
 
